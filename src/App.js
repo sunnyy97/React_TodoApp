e114c020e5043d296b17b5184e6ad336e2c9015a
SUN <siksik2782259@gmail.com>
2024-02-22 14:25:52 +0900

    최종본

--- src/App.js
+++ src/App.js
@@ -1,50 +1,54 @@
-import React, {useState} from 'react';
-import "./App.css"
-import List from './components/List';
-import Form from './components/Form';
+import "./App.css";
+import { useState } from "react";
+import Lists from "./components/Lists";
+import Form from "./components/Form";
 
-export default function App() { 
-  
-  const state = {
-    todoData : [],
-    value : ""
-  }
+const initialTodoData = localStorage.getItem("todoData")
+  ? JSON.parse(localStorage.getItem("todoData"))
+  : [];
 
-  const [todoData, setTodoData] = useState([]);
+function App() {
+  const [todoData, setTodoData] = useState(initialTodoData);
+
   const [value, setValue] = useState("");
 
-
-  const handleSumbit = (e) => {
+  const handleSubmit = (e) => {
     // form 안에 input을 전송할 때 페이지 리로드 되는 걸 막아줌
     e.preventDefault();
-    
+
     // 새로운 할 일 데이터
     let newTodo = {
-      id : Date.now(),
-      title : value,
-      completed : false
-    }
+      id: Date.now(),
+      title: value,
+      completed: false,
+    };
 
     // 원래 있던 할 일에 새로운 할 일 더해주기
     setTodoData((prev) => [...prev, newTodo]);
+    localStorage.setItem("todoData", JSON.stringify([...todoData, newTodo]));
+
+    // 입력란에 있던 글씨 지워주기
     setValue("");
-  }
+  };
 
-   
-    //this.setState({todoData : newTodoData});
+  const handleRemoveClick = () => {
+    setTodoData([]);
+    localStorage.setItem("todoData", JSON.stringify([]));
+  };
 
-    return (
-      <div className='flex items-center justify-center w-screen h-screen bg-blue-100' >
-        <div className='w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg'>
-         <div className = 'flex justify-between mb-3'>
+  return (
+    <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
+      <div className="w-full p-6 m-4 bg-white rounded shadow md:w-3/4 md:max-w-lg lg:w-3/4 lg:max-w-lg">
+        <div className="flex justify-between mb-3">
           <h1>할 일 목록</h1>
+          <button onClick={handleRemoveClick}>Delete All</button>
         </div>
+        <Lists todoData={todoData} setTodoData={setTodoData} />
 
-        <List todoData = {todoData} setTodoData = {setTodoData}/>
-        
-        <Form handleSubmit={handleSumbit} value = {value} setValue={setValue}/>
+        <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
       </div>
-      </div>
-    );
-  
+    </div>
+  );
 }
+
+export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function Form({ handleSubmit, value, setValue }) {
-
   const handleChange = (e) => {
     setValue(e.target.value);
   };
